4478e632e075d947a1bbc5e87f43296b32a37299
Adithya <111501017@smail.iitpkd.ac.in>
2018-11-12 10:37:36 +0800

    contour_detect_with_external_boundary

--- imageProcessing/img_boundary_detect.py
+++ imageProcessing/img_boundary_detect.py
@@ -9,9 +9,14 @@ imgray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
 ret, thresh = cv2.threshold(imgray, 127, 255, 0)
 im2, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
 
-print len(contours)
+cnt = []
 
-cv2.drawContours(im, contours[6], -1, (0,255,0), 3)
+for x in range(len(hierarchy[0])):
+	a,b,c,d = hierarchy[0][x]
+	if d == 0:
+		cnt.append(contours[x])
+
+cv2.drawContours(im, cnt, -1, (0,255,0), 3)
 
 cv2.imshow('contour', im)
 
