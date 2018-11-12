c0d35401301788fe2b42767481675c22229d810a
Adithya <111501017@smail.iitpkd.ac.in>
2018-11-12 16:20:47 +0800

    mod1 workign with mod 2 giving state

--- imageProcessing/class/driver.py
+++ imageProcessing/class/driver.py
@@ -2,6 +2,9 @@ import cv2
 import modules
 
 image = cv2.imread('park_space.jpg')
-
 x_y, r = modules.mod1_ps_init(image, 225, 2)
-modules.mod2_ps_detect(image, 225, x_y, r, 2)
+
+image = cv2.imread('park_space.jpg', 0)
+state = modules.mod2_ps_detect(image, 225, x_y, r, 2)
+
+print state, sum(state)
--- imageProcessing/class/modules.py
+++ imageProcessing/class/modules.py
@@ -1,3 +1,4 @@
+import numpy as np
 import cv_func
 import cv2
 
@@ -5,7 +6,6 @@ def mod1_ps_init(park_image, thres, level):
 	im, (im2, contours, hierarchy) = cv_func.detect_contour(park_image, thres)
 	cnt, x_y, r = cv_func.detect_hierarchy(contours, hierarchy, level)
 
-	print "Press any key to close image"
 	cv_func.display_contour(im, cnt, "detected_parking")
 	cv2.waitKey(0)
 	cv2.destroyAllWindows()
@@ -14,17 +14,22 @@ def mod1_ps_init(park_image, thres, level):
 
 def mod2_ps_detect(park_image, thres, x_y, r, level):
 	cropped_images = cv_func.crop_image(park_image, x_y, 2*r)
+	state = []
 
-	# for x in cropped_images:
-	# cv2.imshow(str(x), x)
-
-	counter = 0
 	for x in cropped_images:
-		im, (im2, contours, hierarchy) = cv_func.detect_contour(x, thres)
-		cnt, x_y, r = cv_func.detect_hierarchy(contours, hierarchy, level)
-		counter = counter + 1
-		cv_func.display_contour(im, cnt, str(counter))
+		s = 0
+		im = x.copy()
+		img = cv2.medianBlur(im,5)
+		# cimg = cv2.cvtColor(im,cv2.COLOR_GRAY2BGR)		
+		
+		circles = cv2.HoughCircles(img,cv2.HOUGH_GRADIENT,1,20,param1=50,param2=30,minRadius=int(0.5*r),maxRadius=int(1.5*r))
+		
+		if circles is not None:
+			s = 1
+			circles = np.round(circles[0, :]).astype("int")
+			for (x, y, r) in circles:
+				cv2.circle(im, (x, y), r, (0, 255, 255), 4)
 
-
-	cv2.waitKey(0)
-	cv2.destroyAllWindows()
+		state.append(s)					
+		
+	return state
