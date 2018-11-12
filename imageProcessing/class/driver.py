bf18b2bb4c9d02c7c34f4439332f5778bac2af4b
immkg <131501018@smail.iitpkd.ac.in>
2018-11-12 22:25:17 +0800

    opencv mods done

--- imageProcessing/class/driver.py
+++ imageProcessing/class/driver.py
@@ -3,7 +3,7 @@ import modules
 
 image = cv2.imread('park_space_uc.jpg')
 x_y_bond = modules.mod0_ps_bond(image, 225, 1)
-x_y_plot, r = modules.mod1_ps_init(image, 225, 2)
+# x_y_plot, r = modules.mod1_ps_init(image, 225, 2)
 
 # image = cv2.imread('park_space_uc.jpg', 0)
 # state = modules.mod2_ps_detect(image, 225, x_y_plot, r, 2)
@@ -13,5 +13,14 @@ x_y_plot, r = modules.mod1_ps_init(image, 225, 2)
 # state = modules.mod2_ps_detect(image, 225, x_y_plot, r, 2)
 # print state, sum(state)
 
-image = cv2.imread('park_space_oc.jpg', 0)
-modules.mod3_ps_object(image, 225, x_y_plot, r, 2)
+# image = cv2.imread('park_space_oc.jpg', 0)
+# modules.mod3_ps_object(image, 225, x_y_plot, r, 2)
+
+# ref = cv2.imread('park_space_uc.jpg')
+# out = cv2.imread('park_space_oc.jpg')
+
+# norm = modules.mod4_psrb_ref(ref, out, x_y_plot, r, 225)
+# print norm
+
+# x_y = [[40, 69], [12, 43], [65, 83]]
+mod_image = modules.mod5_ps_draw(image, x_y_bond)
--- imageProcessing/class/modules.py
+++ imageProcessing/class/modules.py
@@ -16,7 +16,11 @@ def mod0_ps_bond(image, thres, level):
 	cv2.waitKey(0)
 	cv2.destroyAllWindows()
 
-	return cnt[0]
+	x_y_list = []
+	for i in cnt[0]:
+		x_y_list.append([i[0][0], i[0][1]])
+
+	return x_y_list
 
 def mod1_ps_init(image, thres, level):
 	park_image = image.copy()
@@ -69,11 +73,37 @@ def mod3_ps_object(image, thres, x_y, r, level):
 	park_image = image.copy()
 	cropped_images = cv_func.crop_image(park_image, x_y, 2*r)
 
-	# for x in cropped_images:
-	# 	im = x.copy()
-	# 	ci.run_inference_on_image(im)
+	for x in cropped_images:
+		cv2.imwrite('temp.jpg', x)
+		ci.FLAGS, ci.unparsed = arg_parse('temp.jpg')
+		name, score = ci.main(ci.FLAGS)
+		print name, score
 
-	ci.FLAGS, ci.unparsed = arg_parse('2.jpg')
-	name, score = ci.main(ci.FLAGS)
+def mod4_psrb_ref(ref, out, x_y, r, thres):
+	ref_img = ref.copy()
+	out_img = out.copy()
 
-	print name, score
+	ret, ref_thresh = cv2.threshold(ref_img, thres, 255, 0)
+	ret, out_thresh = cv2.threshold(out_img, thres, 255, 0)
+
+	ref_crop = cv_func.crop_image(ref_thresh, x_y, 2*r)
+	out_crop = cv_func.crop_image(out_thresh, x_y, 2*r)
+
+	norm = []
+
+	for x in range(len(ref_crop)):
+		diff = ref_crop[x]-out_crop[x]
+		norm.append(np.linalg.norm(diff))
+	
+	return norm/max(norm)
+
+
+def mod5_ps_draw(image, x_y):
+	park_image = image.copy()
+	for x, y in x_y:
+		cv2.circle(park_image, (x, y), 3, (0,0,255), -1)
+
+	cv2.imshow("tada", park_image)
+	cv2.waitKey(0)
+	cv2.destroyAllWindows()
+	return park_image
